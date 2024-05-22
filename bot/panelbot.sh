8eee7a9898db2e9d374c8bed449045c080a7048a
LT-BACKEND <lunaticvpnscript293@gmail.com>
2024-05-23 01:56:40 +0700

    Add files via upload

---
+++ bot/panelbot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+lunatic_service=$(systemctl status lunatictunneling | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+
+# STATUS SERVICE  SQUID 
+if [[ $lunatic_service == "running" ]]; then 
+   status_lunatictunneling=" ${GREEN}Running ${NC}"
+else
+   status_lunatictunneling="${RED}  Not Running ${NC}"
+fi
+clear
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m               ⇱ bot panel Lunatic ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${LIGHT}"
+echo -e "STATUS LUNATIC BOT: $lunatic_service"
+echo -e "1.START BOT"
+echo -e "2.STOP BOT"
+echo -e "3.Edit bot/id telegram/domain"
+echo -e "x.Back to Menu"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+read -p "PILIH NOMOR:" bro
+
+case $bro in
+01 | 1) clear ; systemctl restart lunatictunneling && panelbot ;;
+02 | 2) clear ; systemctl stop lunatictunneling && panelbot ;;
+03 | 3) clear ; nano /root/lunatictunneling/var.txt ;;
+100) clear ; $up2u ;;
+00 | 0) clear ; menu ;;
+*) clear ; menu ;;
+esac
