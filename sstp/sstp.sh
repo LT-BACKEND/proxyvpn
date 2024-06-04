df07c79c3252b5cfe684fb1890a721c402d147af
LT-BACKEND <lunaticvpnscript293@gmail.com>
2024-06-05 03:01:06 +0700

    Update sstp.sh

--- sstp/sstp.sh
+++ sstp/sstp.sh
@@ -24,7 +24,7 @@ echo -e "${NC}${LIGHT}Telegram : https://t.me/LunaticTunnel"
 exit 0
 fi
 # Link Hosting Kalian
-babatok="raw.githubusercontent.com/AkbarStoreVPN/scriptvps/main/sstp"
+babatok="https://raw.githubusercontent.com/LT-BACKEND/proxyvpn/memek/sstp"
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
@@ -45,9 +45,11 @@ elif [[ "$ver" = "10" ]]; then
 yoi=Debian10
 fi
 fi
-mkdir /home/sstp
+mkdir -p /home/sstp
+mkdir -p /etc/sstp
 touch /home/sstp/sstp_account
-touch /var/lib/akbarstorevpn/data-user-sstp
+touch echo "plughin & account" > /etc/sstp/.sstp.db
+
 #detail nama perusahaan
 country=ID
 state=Indonesia
@@ -67,12 +69,15 @@ make
 cpack -G DEB
 dpkg -i accel-ppp.deb
 mv /etc/accel-ppp.conf.dist /etc/accel-ppp.conf
-wget -O /etc/accel-ppp.conf "https://${babatok}/accel.conf"
+wget -O /etc/accel-ppp.conf "${babatok}/accel.conf"
 sed -i $MYIP2 /etc/accel-ppp.conf
+sed -i $MYIP /etc/accel-ppp.conf
+sed -i $MYIP /etc/accel.conf
 chmod +x /etc/accel-ppp.conf
 systemctl start accel-ppp
 systemctl enable accel-ppp
 #gen cert sstp
+mkdir -p /home/sstp
 cd /home/sstp
 openssl genrsa -out ca.key 4096
 openssl req -new -x509 -days 3650 -key ca.key -out ca.crt \
@@ -81,6 +86,7 @@ openssl genrsa -out server.key 4096
 openssl req -new -key server.key -out ia.csr \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 openssl x509 -req -days 3650 -in ia.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+mkdir -p /home/vps/public_html
 cp /home/sstp/server.crt /home/vps/public_html/server.crt
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 444 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 444 -j ACCEPT
