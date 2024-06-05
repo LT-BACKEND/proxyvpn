5e9b8af695f5b036fe07014a368608598cf09198
LT-BACKEND <lunaticvpnscript293@gmail.com>
2024-06-05 11:15:59 +0700

    Update ipsec.sh

--- ipsec/ipsec.sh
+++ ipsec/ipsec.sh
@@ -13,20 +13,6 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 export PATH="/usr/bin/manager:/usr/bin:/usr/manager:/usr/bin:/manager:/bin"
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/LT-BACKEND/REGISTER/main/IPVPS | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}WhatsApp : 085955333616"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/LunaticTunnel"
-exit 0
-fi
 # ==================================================
 # Link Hosting Kalian
 babatok="raw.githubusercontent.com/LT-BACKEND/proxyvpn/memek/ipsec"
